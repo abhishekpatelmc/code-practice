fcd60f83441f2a69e5bd5a8bf83e2ef40b7e6259
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-11-23 01:28:04 -0500

    explain added

--- book-examples/recursion.js
+++ book-examples/recursion.js
@@ -1,3 +1,7 @@
+// Recurssion:
+// It has two parts : Base case and Recurssion case
+// there should be also a end case whihc will end the recurssion
+
 function countdown(i) {
   if (i <= 0) return;
   else {
--- book-examples/stack.js
+++ book-examples/stack.js
@@ -1,3 +1,6 @@
+// Stack is a type of data structure
+// Stacks are used in recusssion - where call stack is used for executing the recurssion fucntion
+
 function greet1(nam) {
   console.log(`Hello ${nam}`);
 
